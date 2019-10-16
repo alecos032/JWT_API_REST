e25be0dc1591eb1fce767ce82eb2e2b365caf603
Alejandro Romero <alejo-90@hotmail.com>
2019-10-15 19:49:11 -0500

    Add response object class

--- src/main/java/com/co/core/test/controller/ApiController.java
+++ src/main/java/com/co/core/test/controller/ApiController.java
@@ -1,14 +1,18 @@
 package com.co.core.test.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.co.core.test.modelo.ResponseObject;
 
 @RestController
 public class ApiController {
 
 	@GetMapping(value="/demo")
-	public String demo() {
-		return "Welcome to JWT";
+	public ResponseObject demo() {
+		ResponseObject response= new ResponseObject(HttpStatus.OK.toString(),"Welcome to Login with JWT");
+		return response;
 	}
 }
